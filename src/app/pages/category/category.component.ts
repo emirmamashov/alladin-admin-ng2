7e75095307e972cd88b741cbf7bbcac849cc2c1f
Emir Mamashov <emir.mamashov@gmail.com>
2017-07-14 00:22:55 +0600

    add categoryItems primeng

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
+// config
+import { Notify_config } from '../../config';
+
 // models
 import { Category } from '../../models/category';
 import { ResponseApi } from '../../models/response';
+import { Notify } from '../../models/notify';
+import { SelectItem } from 'primeng/primeng';
 
 // services
 import { CategoryService } from '../../services/category.service';
+import { NotifyService } from '../../services/notify.service';
 
 @Component({
   selector: 'app-category',
@@ -18,15 +24,20 @@ import { CategoryService } from '../../services/category.service';
 export class CategoryComponent implements OnInit, OnDestroy {
   categories = new Array<Category>();
   newCategory = new Category();
+  categoriesItem: SelectItem[] = [];
+
   isEdit = false;
 
   getAllConnection: any;
+  addConnection: any;
 
   constructor(
-    private categoryService: CategoryService
+    private categoryService: CategoryService,
+    private notifyService: NotifyService
   ) { }
 
   ngOnInit() {
+    this.categoriesItem.push({ label: 'Выбрите категорию', value: '' });
     this.getAll();
   }
 
@@ -36,6 +47,7 @@ export class CategoryComponent implements OnInit, OnDestroy {
         console.log(response);
         if (response.success) {
           this.categories = response.data.data.categories;
+          this.setCategoriesItem(this.categories);
         }
       },
       (err) => {
@@ -44,9 +56,43 @@ export class CategoryComponent implements OnInit, OnDestroy {
     );
   }
 
+  setCategoriesItem(categories: Array<Category>) {
+    categories.forEach((category) => {
+      this.categoriesItem.push({ label: category.name, value: category._id });
+    });
+  }
+
+  add(category: Category) {
+    const notify = new Notify();
+    notify.type = Notify_config.typeMessage.danger;
+    notify.text = 'Что то пошло не так!';
+
+    this.addConnection = this.categoryService.add(category).subscribe(
+      (response: ResponseApi) => {
+        console.log(response);
+        if (!response.success) {
+          notify.text = response.message;
+          return this.notifyService.addNotify(notify);
+        }
+        this.categories.push(response.data.data.category);
+
+        notify.type = Notify_config.typeMessage.success;
+        notify.text = 'Добавлено!';
+        this.notifyService.addNotify(notify);
+      },
+      (err) => {
+        console.log(err);
+        this.notifyService.addNotify(notify);
+      }
+    );
+  }
+
   ngOnDestroy() {
     if (this.getAllConnection && this.getAllConnection.unsubscribe) {
       this.getAllConnection.unsubscribe();
+    }
+    if (this.addConnection && this.addConnection.unsubscribe) {
+      this.addConnection.unsubscribe();
     }
   }
 
